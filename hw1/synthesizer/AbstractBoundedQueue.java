3b161b5f6341400f22ebe3eea0bf9edc7888045f
Fengyu He <banxiabian@gmail.com>
2020-06-01 17:20:59 -0400

    1st time upload

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,18 @@
+package synthesizer;
+
+/**
+ * Created by Fengyu He on 5/25/2020
+ * @param <T>
+ */
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T>{
+    protected int fillCount;
+    protected int capacity;
+    public int capacity(){
+        return capacity;
+    }
+
+    public int fillCount(){
+        return fillCount;
+    }
+}
---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,102 @@
+package synthesizer;
+import synthesizer.AbstractBoundedQueue;
+
+import java.util.Iterator;
+
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        this.capacity = capacity;
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        rb = (T[]) new Object[capacity];
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    public void enqueue(T x) {
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        last += 1;
+        if (last == capacity) {
+            last = 0;
+        }
+        fillCount += 1;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer Underflow");
+        }
+        T removed = rb[first];
+        rb[first] = null;
+        first += 1;
+        if (first == capacity) {
+            first = 0;
+        }
+        fillCount -= 1;
+        return removed;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        return rb[first];
+
+    }
+
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+
+    private class ArrayRingBufferIterator implements Iterator<T> {
+
+         private int ptr;
+
+         public ArrayRingBufferIterator() {
+             ptr=first;
+    }
+
+        @Override
+        public boolean hasNext() {
+            return (ptr != last);
+        }
+
+        @Override
+        public T next(){
+             T returnItem = rb[ptr];
+             ptr+=1;
+            if (ptr == capacity) {
+                ptr = 0;
+            }
+            return returnItem;
+        }
+    }
+}
+
