c43468f4f014463447ad7c84658e68ead9c731cc
Fengyu He <banxiabian@gmail.com>
2020-08-11 15:51:34 -0400

    complete part1

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  * This class provides all code necessary to take a query box and produce
@@ -9,8 +11,47 @@ import java.util.Map;
  */
 public class Rasterer {
 
+    double lrlon, ullon, w, h, ullat, lrlat, LonDPP, maxLon;
+    int rowLeft, rowRight, columnUp, columnDown;
+    String[][] render_grid;
+    int depth;
+    Boolean query_success;
+
+    private static final int SL = 288200;
+
     public Rasterer() {
-        // YOUR CODE HERE
+
+    }
+
+    /**
+     * 给定文件名，计算对应的经纬度边界
+     */
+    private Map<String, Double> fileBounds(String file) {
+        String pattern = "d([0-9])_x([0-9]+)_y([0-9]+)"; //匹配缩放等级、横纵坐标的正则表达式
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(file);
+        int D = 0, xLevel = 0, yLevel = 0;
+        if (m.find()) {
+            D = Integer.parseInt(m.group(1));
+            xLevel = Integer.parseInt(m.group(2));
+            yLevel = Integer.parseInt(m.group(3));
+        }
+
+        double ullon = MapServer.ROOT_ULLON + (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON)
+                * (xLevel / Math.pow(2, D));
+        double lrlon = MapServer.ROOT_ULLON + (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON)
+                * ((xLevel + 1) / Math.pow(2, D));
+        double ullat = MapServer.ROOT_ULLAT - (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT)
+                * (yLevel / Math.pow(2, D));
+        double lrlat = MapServer.ROOT_ULLAT - (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT)
+                * ((yLevel + 1) / Math.pow(2, D));
+
+        Map<String, Double> toReturn = new HashMap<>();
+        toReturn.put("ullon", ullon);
+        toReturn.put("lrlon", lrlon);
+        toReturn.put("ullat", ullat);
+        toReturn.put("lrlat", lrlat);
+        return toReturn;
     }
 
     /**
@@ -42,10 +83,85 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+        System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+
+        depth = 0;
+        lrlon = params.get("lrlon");
+        ullon = params.get("ullon");
+        w = params.get("w");
+        h = params.get("h");
+        ullat = params.get("ullat");
+        lrlat = params.get("lrlat");
+        LonDPP = (lrlon - ullon) * SL / w;
+        maxLon = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / MapServer.TILE_SIZE * SL;
+        while( !(maxLon / Math.pow(2,depth) <= LonDPP || depth == 7)) {
+            depth += 1;
+        }
+
+        double lonPerTile = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / Math.pow(2, depth);
+        //计算一行中最左端Tile的坐标
+        if (ullon < MapServer.ROOT_ULLON) {
+            rowLeft = 0;
+        } else {
+            rowLeft = (int) Math.floor((ullon - MapServer.ROOT_ULLON) / lonPerTile);
+        }
+        //计算一行中最右端Tile的坐标
+        rowRight = (int) (Math.pow(2, depth) - 1 - Math.floor((MapServer.ROOT_LRLON
+                - lrlon) / lonPerTile));
+        if (lrlon > MapServer.ROOT_LRLON) {
+            rowRight = (int) (Math.pow(2, depth) - 1);
+        }
+        int columnNum = rowRight - rowLeft + 1; //列数
+
+        double latPerTile = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / Math.pow(2, depth);
+        //计算一列中最上方Tile的坐标
+        if (ullat > MapServer.ROOT_ULLAT) {
+            columnUp = 0;
+        } else {
+            columnUp = (int) Math.floor((MapServer.ROOT_ULLAT - ullat) / latPerTile);
+        }
+        //计算一列中最下方Tile的坐标
+        columnDown = (int) (Math.pow(2, depth) - 1 - Math.floor((lrlat
+                - MapServer.ROOT_LRLAT) / latPerTile));
+        if (lrlat < MapServer.ROOT_LRLAT) {
+            columnDown = (int) (Math.pow(2, depth) - 1);
+        }
+        int rowNum = columnDown - columnUp + 1; //行数
+
+        String[][] gridFiles = new String[rowNum][columnNum]; //包含文件名的二维数组
+        for (int j = 0; j < rowNum; j++) {
+            for (int i = 0; i < columnNum; i++) {
+                gridFiles[j][i] = "d" + depth + "_x" + (rowLeft + i) + "_y"
+                        + (columnUp + j) + ".png";
+            }
+        }
+
+        //计算边界的经纬度
+        double ullon = fileBounds("d" + depth + "_x" + rowLeft + "_y"
+                + columnUp + ".png").get("ullon");
+        double ullat = fileBounds("d" + depth + "_x" + rowLeft + "_y"
+                + columnUp + ".png").get("ullat");
+        double lrlon = fileBounds("d" + depth + "_x" + rowRight + "_y"
+                + columnDown + ".png").get("lrlon");
+        double lrlat = fileBounds("d" + depth + "_x" + rowRight + "_y"
+                + columnDown + ".png").get("lrlat");
+
+        results.put("depth", depth);
+        results.put("render_grid", gridFiles);
+        results.put("raster_ul_lon", ullon);
+        results.put("raster_ul_lat", ullat);
+        results.put("raster_lr_lon", lrlon);
+        results.put("raster_lr_lat", lrlat);
+        //异常情况时query_success设为false
+        if (params.get("lrlon") < params.get("ullon") || params.get("ullat")
+                < params.get("lrlat")) {
+            results.put("query_success", false);
+        } else {
+            results.put("query_success", true);
+        }
+
+
         return results;
     }
 
